3db0c11daa8128c95ee8955cc518fff9b5fa4eed
vvolfkind <rodolfo.raskovsky85@gmail.com>
2018-10-19 17:00:21 -0300

    api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('movies', function() {
+    return Movie::all();
+});
+
+Route::get('movies/{id}', function($id){
+    return Movie::find($id);
+});
